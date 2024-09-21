70dd31a8a08853f035c7290d4a6a445f32023998
sujanrumakantha@gmail.com <sujanrumakantha@gmail.com>
2024-09-21 17:04:51 +0530

    modified mbile navbar and add new test bottom navigation bar

--- src/manager/index.jsx
+++ src/manager/index.jsx
@@ -3,11 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { setLoading } from "../redux/slices/UniversalLoader";
-import { IoCloseSharp } from "react-icons/io5";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 import { IoExitOutline } from "react-icons/io5";
 import { IoMdMenu } from "react-icons/io";
 import { setUserDetails } from "../redux/slices/UserDetailsSlice";
 import apiClient from "../api/apiClient";
+import { IoSearch } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
+import { RiDashboard2Line } from "react-icons/ri";
+import { ImOffice } from "react-icons/im";
+import { FaUser } from "react-icons/fa";
 import "./style.css";
 import { handleWarningModel } from "../redux/slices/UserSlice";
 
@@ -15,6 +20,7 @@ const Manager = () => {
   const { user } = useSelector((state) => state.userSlice);
   const [loggedInUser, setLoggedInUser] = useState({});
   const [showMenu, setShowMenu] = useState(false);
+  const [activeNavBtn, setActiveNavBtn] = useState("home");
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -37,7 +43,7 @@ const Manager = () => {
   };
 
   return (
-    <>
+    <div className="manager_dashboard_main_container">
       <div className="manager_navbar_container">
         <nav className="manager_navbar_nav">
           <div>
@@ -48,7 +54,9 @@ const Manager = () => {
             </p>
           </div>
           <div className="manager_navbar_nav_desktop_view_links">
-            <p className="manager_navbar_nav_desktop_view_links_active">Dashboard</p>
+            <p className="manager_navbar_nav_desktop_view_links_active">
+              Dashboard
+            </p>
             <p>Contact Us</p>
             <p>Logout</p>
           </div>
@@ -69,24 +77,21 @@ const Manager = () => {
             <div>
               <section>
                 <span>
-                  <img
-                    src="https://sarayuinfotech.in/images/sarayu/Logo/sarayu-comp-icon.png"
-                    alt="sarayu logo"
-                  />
-                </span>
-                <span>
-                  <IoCloseSharp
-                    size={"30px"}
+                  <MdOutlineKeyboardDoubleArrowRight
+                    size={"20px"}
                     style={{ cursor: "pointer" }}
-                    className="mb-2"
+                    className="close_mobile_menu_icon"
                     onClick={() => setShowMenu(false)}
                   />
                 </span>
+                <span>
+                  {/* <img
+                    src="https://sarayuinfotech.in/images/sarayu/Logo/sarayu-comp-icon.png"
+                    alt="sarayu logo"
+                  /> */}
+                  SARAYU
+                </span>
               </section>
-              <div className="manager_navbar_nav_mobile_view_links_container">
-                <p>Dashboard</p>
-                <p>Contact Us</p>
-              </div>
               <footer
                 className="manager_navbar_nav_mobile_view_links_footer"
                 onClick={() => dispatch(handleWarningModel())}
@@ -99,11 +104,60 @@ const Manager = () => {
                   <div>{loggedInUser?.email}</div>
                 </div>
               </footer>
+              <div className="manager_navbar_nav_mobile_view_links_container">
+                <p>Dashboard</p>
+                <p>Contact Us</p>
+              </div>
             </div>
           </div>
         </section>
       </div>
-    </>
+      <div className="footer_navigationbar_mobile_view">
+        <div
+          className={
+            activeNavBtn === "home" && `footer_navigationbar_mobile_view_active`
+          }
+        >
+          <FaHome onClick={() => setActiveNavBtn("home")} />
+          {activeNavBtn === "home" && <span>Home</span>}
+        </div>
+        <div
+          className={
+            activeNavBtn === "graph" &&
+            `footer_navigationbar_mobile_view_active`
+          }
+        >
+          <RiDashboard2Line onClick={() => setActiveNavBtn("graph")} />
+          {activeNavBtn === "graph" && <span>Graph</span>}
+        </div>
+        <div
+          className={
+            activeNavBtn === "search" &&
+            `footer_navigationbar_mobile_view_active`
+          }
+        >
+          <IoSearch onClick={() => setActiveNavBtn("search")} />
+          {activeNavBtn === "search" && <span>Search</span>}
+        </div>
+        <div
+          className={
+            activeNavBtn === "office" &&
+            `footer_navigationbar_mobile_view_active`
+          }
+        >
+          <ImOffice onClick={() => setActiveNavBtn("office")} />
+          {activeNavBtn === "office" && <span>Office</span>}
+        </div>
+        <div
+          className={
+            activeNavBtn === "user" && `footer_navigationbar_mobile_view_active`
+          }
+        >
+          <FaUser onClick={() => setActiveNavBtn("user")} />
+          {activeNavBtn === "user" && <span>User</span>}
+        </div>
+      </div>
+    </div>
   );
 };
 
