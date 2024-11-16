4aceb6f8595c168be17cdfc8ee950971fbd3dd06
sujanrumakantha@gmail.com <sujanrumakantha@gmail.com>
2024-11-16 10:12:46 +0530

    updated api url

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://sarayu-node-backend.onrender.com/api/v1",
+  baseURL: "https://sarayu-node-backend-pmuw.onrender.com/api/v1",
 });
 
 export default apiClient;
