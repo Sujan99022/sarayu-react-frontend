195e2775b0664354c7216057d80b7516f1026b88
sujanrumakantha@gmail.com <sujanrumakantha@gmail.com>
2024-12-30 12:27:28 +0530

    modified watch list interface

--- src/users/body/components/Favorite.jsx
+++ src/users/body/components/Favorite.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "../../style.css";
-import { useSelector } from "react-redux";
-import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
 import apiClient from "../../../api/apiClient";
-import { CiBookmarkRemove } from "react-icons/ci";
-import SmallGraph from "../graphs/smallgraph/SmallGraph";
+import { setUserDetails } from "../../../redux/slices/UserDetailsSlice";
+import { toast } from "react-toastify";
 import Loader from "../../loader/Loader";
-import { FaRegEye } from "react-icons/fa";
-import DigitalViewOne from "./../graphs/digitalview/DigitalViewOne";
+import LiveDataTd from "../common/LiveDataTd";
+import { IoMdRemoveCircle } from "react-icons/io";
+import { BiSolidReport } from "react-icons/bi";
+import WeekTd from "../common/WeekTd";
+import YestardayTd from "../common/YestardayTd";
+import TodayTd from "../common/TodayTd";
 import { VscGraph } from "react-icons/vsc";
 import { FaDigitalOcean } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
-const Favorite = () => {
-  const { user } = useSelector((state) => state.userSlice);
+const Dashboard = () => {
   const [loggedInUser, setLoggedInUser] = useState({});
   const [localLoading, setLocalLoading] = useState(false);
-  const [activeFavBtn, setActiveFavBtn] = useState("graph");
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userSlice);
+  const [favoriteList, setFavoriteList] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -30,30 +34,26 @@ const Favorite = () => {
     try {
       const res = await apiClient.get(`/auth/${user.role}/${user.id}`);
       setLoggedInUser(res?.data?.data);
+      dispatch(setUserDetails(res?.data?.data));
+      setFavoriteList(res?.data?.data?.favorites);
+      setLocalLoading(false);
     } catch (error) {
-      toast.error(
-        error?.response?.data?.error || "Failed to fetch user details"
-      );
-    } finally {
+      toast.error(error?.response?.data?.error);
       setLocalLoading(false);
     }
   };
 
-  const removeFromFavorite = async (topic) => {
+  const handleRemoveFavorite = async (topic) => {
     try {
       await apiClient.delete(`/auth/${user.role}/${user.id}/favorites`, {
         data: { topic },
       });
-
-      setLoggedInUser((prev) => ({
-        ...prev,
-        favorites: prev.favorites.filter((fav) => fav !== topic),
-      }));
-      // toast.success("Topic removed from favorites");
-      window.location.reload();
+      setFavoriteList((prev) => prev.filter((fav) => fav !== topic));
+      toast.success("Topic removed from watchlist");
+      fetchUserDetails();
     } catch (error) {
       toast.error(
-        error?.response?.data?.error || "Failed to remove topic from favorites"
+        error?.response?.data?.error || "Failed to remove topic from watchlist"
       );
     }
   };
@@ -61,70 +61,82 @@ const Favorite = () => {
   if (localLoading) {
     return <Loader />;
   }
-
   return (
-    <div className="allusers_favorites_main_container">
-      <div className="allusers_favorites_main_btn_container">
-        <div>
-          <button
-            className={
-              activeFavBtn === "graph" && "allusers_favorites_main_active_btn"
-            }
-            onClick={() => setActiveFavBtn("graph")}
-          >
-            Graph <VscGraph />
-          </button>
-          <button
-            className={
-              activeFavBtn === "digital" && "allusers_favorites_main_active_btn"
-            }
-            onClick={() => setActiveFavBtn("digital")}
-          >
-            DigitalView <FaDigitalOcean />
-          </button>
+    <div className="allusers_dashboard_main_container">
+      <div className="alluser_alloperators_container">
+        <div className="alluser_alloperators_scrollable-table">
+          <table className="alluser_alloperators_table">
+            <thead>
+              <tr>
+                <th style={{ background: "red" }}>Tag name</th>
+                <th>Week's max</th>
+                <th>Yesterday's max</th>
+                <th>Today's max</th>
+                <th className="allusers_dashboard_live_data_th">Live</th>
+                <th>Unit</th>
+                <th>Report</th>
+                <th>Graph/Digital</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {loggedInUser?.favorites?.map((item, index) => (
+                <tr key={index}>
+                  <td style={{ background: "#34495e", color: "white" }}>
+                    {item?.split("/")[2]}
+                  </td>
+                  <WeekTd topic={item} />
+                  <YestardayTd topic={item} />
+                  <TodayTd topic={item} />
+                  <LiveDataTd topic={item} />
+                  <td>V</td>
+                  <td>
+                    <BiSolidReport
+                      onClick={() => {
+                        const encodedTopic = encodeURIComponent(item);
+                        navigate(`/allusers/report/${encodedTopic}`);
+                      }}
+                      size={"20"}
+                      style={{ cursor: "pointer" }}
+                      color="gray"
+                    />
+                  </td>
+                  <td className="allusers_dashboard_graph_digital_td">
+                    <button
+                      onClick={() => {
+                        const encodedTopic = encodeURIComponent(item);
+                        navigate(`/allusers/viewsinglegraph/${encodedTopic}`);
+                      }}
+                    >
+                      <VscGraph />
+                    </button>
+                    <button
+                      onClick={() => {
+                        const encodedTopic = encodeURIComponent(item);
+                        navigate(
+                          `/allusers/singledigitalmeter/${encodedTopic}/${user.role}/${user.id}`
+                        );
+                      }}
+                    >
+                      <FaDigitalOcean />
+                    </button>
+                  </td>
+                  <td>
+                    <IoMdRemoveCircle
+                      color="red"
+                      size={"20"}
+                      style={{ cursor: "pointer" }}
+                      onClick={() => handleRemoveFavorite(item)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-      {activeFavBtn === "graph" && (
-        <div className="allusers_favorites_graph_container">
-          <div className="users_small_graphs_primary_container">
-            {loggedInUser?.favorites?.map((item, index) => (
-              <div
-                key={index}
-                className="users_small_graphs_secondary_container"
-              >
-                <div className="users_graphs_view_edit_icon_container">
-                  <div onClick={() => removeFromFavorite(item)}>
-                    <CiBookmarkRemove size={"20"} />
-                  </div>
-                  <div
-                    onClick={() => {
-                      const encodePrams = encodeURIComponent(item);
-                      navigate(`/allusers/viewsinglegraph/${encodePrams}`);
-                    }}
-                  >
-                    <FaRegEye size={"20"} />
-                  </div>
-                </div>
-                <div className="users_graphs_topic_name">
-                  <p>{item?.split("/")[2]}</p>
-                </div>
-                <SmallGraph topic={item} height={220} />
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-      {activeFavBtn === "digital" && (
-        <div className="allusers_favorites_digitalmeter_container">
-          {loggedInUser?.favorites?.map((item, index) => (
-            <div key={index}>
-              <DigitalViewOne topic={item} />
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
 
-export default Favorite;
+export default Dashboard;
